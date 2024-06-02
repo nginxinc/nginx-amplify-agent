6d3e17d8c75029df47d65e2acc5194e0a81c8bf8
Andrei Belov <a.belov@f5.com>
2024-06-02 18:44:16 +0400

    Update agent version to 1.8.2
    Changes:
     - pyMySQL version updated to 1.1.1 (CVE-2024-36039),
     - requests version updated to 2.32.2,
     - dropped support for RHEL 8 (Python 3.6 is EOL since 2021).

--- amplify/agent/common/context.py
+++ amplify/agent/common/context.py
@@ -37,7 +37,7 @@ class Context(Singleton):
 
         # define vars
         self.cpu_last_check = 0
-        self.version_semver = (1, 8, 1)
+        self.version_semver = (1, 8, 2)
         self.version_build = 1
         self.uuid = None
         self.version = '%s-%s' % ('.'.join(map(str, self.version_semver)), self.version_build)
